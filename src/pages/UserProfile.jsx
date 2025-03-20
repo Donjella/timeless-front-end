2ddb7088c6902f250e316246602e7bc37af61adf
CoderStee <15881@coderacademy.edu.au>
2025-03-20 16:13:39 +1100

    feat: UserProfile rewrite - setting up file structure

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthData } from '../hooks/useAuthData';
+import { api } from '../utils/api';
+
+
+
+
+export function UserProfile (){
+
+const navigate = useNavigate();
+
+
+const { authData }
+
+
+
+
+
+
+
+
+
+}
+
+export default UserProfile;
